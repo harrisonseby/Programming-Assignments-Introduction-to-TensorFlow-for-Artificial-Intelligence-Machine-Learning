e2275ca17ea8e74857741f85b68ddadb3f720121
Harrison Seby <69869649+harrisonseby@users.noreply.github.com>
2020-12-17 21:38:12 +0530

    Update Exercise_1_housing_prices.py

--- Exercise_1_housing_prices.py
+++ Exercise_1_housing_prices.py
@@ -9,7 +9,9 @@ def house_model(y_new):
     xs=np.array([1.0,2.0,3.0,4.0,5.0],dtype=float)
     ys=np.array([1.0,1.5,2.0,2.5,3.0],dtype=float)
     model=tf.keras.Sequential([keras.layers.Dense(units=1,input_shape=[1])])
-    model.compile(optimizer='sgd',loss='mean_squared_error')
+    
+    # here 'sgd' is Stochastic Gradient descent
+    model.compile(optimizer='sgd',loss='mean_squared_error') 
     model.fit(xs,ys,epochs=500)
     return model.predict(y_new)[0]
 
